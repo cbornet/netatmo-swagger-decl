c330348c9d5538b9e0338be10ad8935db7cdb691
cbornet <cbornet@hotmail.com>
2017-10-25 12:28:35 +0000

    Adapt swagger-ui for Netatmo

--- scripts/build.js
+++ scripts/build.js
@@ -13,7 +13,10 @@ exec('npm run swagger bundle --        -o web_deploy/swagger.json');
 exec('npm run swagger bundle -- --yaml -o web_deploy/swagger.yaml');
 
 var SWAGGER_UI_DIST = Path.dirname(require.resolve('swagger-ui'));
-rm('-rf', 'web_deploy/swagger-ui/')
-cp('-R', SWAGGER_UI_DIST, 'web_deploy/swagger-ui/')
-sed('-i', 'http://petstore.swagger.io/v2/swagger.json', '../swagger.json', 'web_deploy/swagger-ui/index.html')
-
+rm('-rf', 'web_deploy/swagger-ui/');
+cp('-R', SWAGGER_UI_DIST, 'web_deploy/swagger-ui/');
+sed('-i', 'http://petstore.swagger.io/v2/swagger.json', '../swagger.json', 'web_deploy/swagger-ui/index.html');
+sed('-i', 'clientId: "your-client-id"', 'clientId: "55744f211977595c67874864"', 'web_deploy/swagger-ui/index.html');
+sed('-i', 'clientSecret: "your-client-secret-if-required"', 'clientSecret: "2j9eANMyxorN7z1EGL9Jnf2N98vavLSPwwKLNkZxluj"', 'web_deploy/swagger-ui/index.html');
+sed('-i', 'appName: "your-app-name"', 'appName: "Netatmo swagger"', 'web_deploy/swagger-ui/index.html');
+sed('-i', '"Authorization","Bearer "\\+n,"header"', "'access_token', n, 'query'", 'web_deploy/swagger-ui/lib/swagger-oauth.js');
